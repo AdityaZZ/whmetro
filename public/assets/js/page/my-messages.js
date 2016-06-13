d6fc5576aa213418d72c54af6e5d887cc75bff06
gwc0721 <gwc0721@qq.com>
2016-06-13 11:55:08 +0800

    初步完成 我的消息-风险 页的数据对接

--- public/assets/js/page/my-messages.js
+++ public/assets/js/page/my-messages.js
@@ -6,33 +6,155 @@ var MyMessages = (function($) {
     defaults: {
       message: {
         top_right_corner: '',
-        enable_comment: true,
+        enable_comment: false,
         show_comment_list: false,
-        more_comments_btn: true
+        more_comments_btn: false
       },
       comment: {}
     },
     message: {
-      data: {},
       map: {},
-      has_more: true
+      translate: function() {},
+      load: {
+        url: '',
+        method: 'GET',
+        enctype: 'application/x-www-form-urlencoded',
+        data: {}
+      },
+      acknowledge: {
+        url: '',
+        method: 'POST',
+        enctype: 'application/x-www-form-urlencoded',
+        data: {}
+      },
+      offset: undefined
     },
     comment: {
-      map: {}
+      map: {},
+      translate: function() {},
+      load: {
+        url: '',
+        method: 'GET',
+        enctype: 'application/x-www-form-urlencoded',
+        data: {}
+      },
+      reply: {
+        url: '',
+        method: 'POST',
+        enctype: 'application/x-www-form-urlencoded',
+        data: {}
+      }
     }
   };
 
   // Templates
-  var tpl_message = $('#template-message').html() || '';
-  var tpl_comment = $('#template-comment').html() || '';
+  var tpl_message = $('#template-message').html() || '<li class="message__item media{% if notice %} notice{% endif %}" data-id="{{id}}" data-time="{{timestamp}}">\
+  <div class="media-left">\
+    {% if author_url %}<a href="{{author_url}}">{% endif %}<img class="img-circle" src="{{avatar_url}}">{% if author_url %}</a>{% endif %}\
+  </div>\
+  <div class="media-body">\
+    <h4 class="media-heading">{% if author_url %}<a href="{{author_url}}">{% endif %}{{author}}{% if author_url %}</a>{% endif %}<small>{{date}}</small></h4>\
+    <div class="top-right-corner">\
+      {{top_right_corner | safe}}\
+    </div>\
+    <div class="message__item-content{% if shrink %} shrinked{% endif %}">\
+      {% if heading %}<div class="message__item-content__heading">{{heading}}</div>{% endif %}\
+      {{message | safe}}\
+      <div class="message__item-mask"></div>\
+      <a href="#" class="collapse-button"></a>\
+    </div>\
+    {% if reply_quote %}\
+    <div class="reply-quote" data-label="回复我的评论：">{{reply_quote}}</div>\
+    {% endif %}\
+    <ul class="message__labels">\
+      {% for label in labels %}\
+      <li class="label {{label.type}}" data-id="{{label.id}}">{% if label.url %}<a href="{{label.url}}">{% endif %}{{label.name}}{% if label.url %}</a>{% endif %}</li>\
+      {% endfor %}\
+    </ul>\
+    {% if acknowledge %}<button type="button" class="btn btn-primary btn-lg btn-block message__ack-btn">确认收到</button>{% endif %}\
+  </div>\
+  {% if enable_comment %}\
+  <div class="comment-box">\
+    <form class="comment-box__form" action="#" method="post">\
+      <img class="img-circle" src="{{avatar_url}}">\
+      <div class="input-group">\
+        <div class="input-wrapper">\
+          <div class="reply-quote" data-label=""></div>\
+          <textarea class="form-control" name="comment" rows="1" maxlength="255" placeholder="回复{{author}}"></textarea>\
+          <button type="button" class="close" aria-label="取消"><span aria-hidden="true">&times;</span></button>\
+        </div>\
+        <span class="input-group-btn">\
+          <button type="button" class="btn btn-default comment-box__image-btn"><i class="fa fa-picture-o"></i></button>\
+          <button type="submit" class="btn btn-default comment-box__reply-btn">回复</button>\
+        </span>\
+      </div>\
+      <div class="image-box">\
+        <ul></ul>\
+        <a href="#" class="add-image"><i class="fa fa-plus"></i></a>\
+      </div>\
+    </form>\
+    {% if show_comment_list %}\
+    <ul class="comment-box__list media-list"></ul>\
+    {% if more_comments_btn %}\
+    <div class="pagination-wrapper"></div>\
+    <a href="#" class="comment-box__show-more">点击查看更多</a>\
+    <a href="#" class="comment-box__show-less">点击收起评论列表</a>\
+    {% endif %}\
+    {% endif %}\
+  </div>\
+  {% endif %}\
+</li>';
+  var tpl_imgitem = $('#template-imgitem').html() || '<li><a href="#"><img src="{{src}}"></a></li>';
+  var tpl_comment = $('#template-comment').html() || '<li class="comment-box__item media" data-id="{{id}}" data-time="{{timestamp}}">\
+  <div class="media-left">\
+    {% if author_url %}<a href="{{author_url}}">{% endif %}<img class="img-circle" src="{{avatar_url}}">{% if author_url %}</a><a href="{{author_url}}">{% endif %}{{author}}{% if author_url %}</a>{% endif %}\
+  </div>\
+  <div class="media-body">\
+    <div class="comment-box__item-main">\
+      <div class="reply-name">{% if reply_to %}{% if reply_to.profile_url %}<a href="{{reply_to.profile_url}}">{% endif %}{{reply_to.name}}{% if reply_to.profile_url %}</a>{% endif %}{% endif %}</div>\
+      <div class="comment-box__item-content">{{message | safe}}</div>\
+    </div>\
+    <div class="comment-box__item-toolbox">\
+      <span class="time">{{time}}</span>\
+      <a href="#" class="reply-link">回复</a>\
+    </div>\
+  </div>\
+</li>';
   if(window.nunjucks) {
     tpl_message = nunjucks.compile(tpl_message);
+    tpl_imgitem = nunjucks.compile(tpl_imgitem);
     tpl_comment = nunjucks.compile(tpl_comment);
   } else {
-    tpl_message.__proto__.render = tpl_comment.__proto__.render = function() {
+    tpl_message.__proto__.render =
+    tpl_imgitem.__proto__.render =
+    tpl_comment.__proto__.render = function() {
       return false;
     }
   }
+
+  function timestamp_to_datetime(timestamp, format) {
+    var object;
+    format = format || 'YYYY-MM-DD HH:mm:ss';
+    if(timestamp > 10000000000) {
+      object = moment(timestamp);
+    } else {
+      object = moment.unix(timestamp);
+    }
+    return object.format(format);
+  }
+  window.timestamp_to_datetime = timestamp_to_datetime;
+
+  function timestamp_to_date(timestamp, format) {
+    var object;
+    format = format || 'YYYY-MM-DD';
+    if(timestamp > 10000000000) {
+      object = moment(timestamp);
+    } else {
+      object = moment.unix(timestamp);
+    }
+    return object.format(format);
+  }
+  window.timestamp_to_date = timestamp_to_date;
 
   function access_object_by_path(object, path) {
     path = path.replace(/\[(\w+)\]/g, '.$1');
@@ -56,10 +178,36 @@ var MyMessages = (function($) {
     $('.message__load-more').on('click', function(e) {
       e.preventDefault();
       var $btn = $(this).blur();
-      if($btn.hasClass('disabled')) {
+      if($btn.hasClass('disabled') || $btn.hasClass('loading')) {
         return;
       }
-      // TODO
+      $btn.addClass('loading');
+      var settings = options.message.load;
+      var request_payload = $.extend({}, settings.data, {
+        offset: options.message.offset
+      });
+      request(settings.method, settings.url, settings.enctype, request_payload, function(data) {
+        $btn.removeClass('loading');
+        var messages = [];
+        options.message.offset = data.offset;
+        if(!data.object.length) {
+          return $btn.addClass('disabled');
+        }
+        $.each(data.object, function() {
+          // var params = translate_object(this, options.message.map, options.defaults.message);
+          var params = $.extend({}, options.defaults.message, options.message.translate(this));
+          var html = tpl_message.render(params);
+          var $message = $(html);
+          $message.find('.comment-box__list').append($.map(data.object.complexComments, function(comment) {
+            var params = $.extend({}, options.defaults.comment, options.comment.translate(comment));
+            return tpl_comment.render(params);
+          }));
+          messages.push($message);
+        });
+        $('ul.message').append(messages);
+      }, function(error) {
+        $btn.removeClass('loading');
+      });
     });
   }
 
@@ -72,12 +220,17 @@ var MyMessages = (function($) {
         return false;
       }
       $ack_btn.text('确认中...').prop('disabled', true);
-      // TODO
-      request('POST', '/message/acknowledge', {
+      var settings = options.message.acknowledge;
+      var request_payload = $.extend({}, settings.data, {
         id: $msg_item.data('id')
-      }, function(data) {
+      });
+      request(settings.method, settings.url, settings.enctype, request_payload, function(data) {
         $ack_btn.addClass('hidden');
         $msg_item.removeClass('notice');
+        alert(data.message);
+      }, function(error, jqxhr, textStatus) {
+        $ack_btn.text('确认收到').prop('disabled', false);
+        alert(error.message || error);
       });
     });
   }
@@ -92,6 +245,7 @@ var MyMessages = (function($) {
 
   function init_image_box() {
     $('.message').on('click', '.comment-box__image-btn', function(e) {
+      e.preventDefault();
       var $img_btn = $(this).blur();
       var $msg_item = $img_btn.closest('.message__item');
       var $img_box = $msg_item.find('.image-box');
@@ -109,7 +263,6 @@ var MyMessages = (function($) {
 
   function init_comment_input() {
     var lastWhich;
-
     $('.message').on('keypress', 'textarea', function(e) {
       if(e.which == 13 || e.which == 10 && e.ctrlKey) { // Enter / Ctrl + Enter
         e.preventDefault();
@@ -186,13 +339,15 @@ var MyMessages = (function($) {
         $comment_box.data('initial_comments', $comment_box.find('.comment-box__list').html());
       }
       // TODO
-      $comment_box.splitpage({
-        url: '/message/comments',
-        type: 'GET',
-        contentType: 'application/json'
-      }, {
-        msgid: $comment_box.closest('.message__item').attr('data-id'),
+      var settings = options.comment.load;
+      var payload = $.extend({}, settings.data, {
+        msgid: $comment_box.closest('.message__item').attr('data-id')
       });
+      $comment_box.splitpage({
+        url: settings.url,
+        type: settings.method,
+        contentType: 'application/json'
+      }, payload);
     }).on('click', '.comment-box__show-less', function(event) {
       event.preventDefault();
       var $comment_box = $(this).blur().closest('.comment-box').removeClass('with-pagination');
@@ -216,47 +371,69 @@ var MyMessages = (function($) {
 
   function translate_object(data, map, defaults) {
     var result = $.extend({}, defaults, data);
-    $.each(map, function(key, value) {
+    $.each(map, function _translate(key, value, object) {
+      if(!object) {
+        object = result;
+      }
       if(typeof value == 'function') {
-        result[key] = value.call(null, result);
+        object[key] = value.call(null, object);
+      } else if(typeof value == 'string') {
+        if(value) {
+          object[key] = access_object_by_path(object, value);
+        } else {
+          object[key] = '';
+        }
+        if(object[key] === undefined) {
+          object[key] = value;
+        }
+      } else if($.isPlainObject(value)) {
+        for(var _k in value) {
+          if(!value.hasOwnProperty(_k)) continue;
+          if(!object[key]) object[key] = {};
+          _translate(_k, value[_k], object[key]);
+        }
       } else {
-        result[key] = access_object_by_path(result, map);
+        object[key] = value;
       }
     });
     return result;
   }
 
-  function request(method, url, data, callback) {
-    if($.isPlainObject(data)) {
+  function request(method, url, enctype, data, callback, errhandler) {
+    if(method.toUpperCase() != 'GET' && enctype == 'application/json' && $.isPlainObject(data)) {
       data = JSON.stringify(data);
     }
     $.ajax({
       url: url,
       type: method,
-      contentType: 'application/json',
+      contentType: enctype,
       dataType: 'json',
       data: data,
       complete: function(jqXHR, textStatus) {
-        if(jqXHR.status != 200) {
+        if(jqXHR.status != 200 || jqXHR.responseJSON && jqXHR.responseJSON.code != '200') {
           // TODO: advanced error handling
-          var error = jqXHR.responseText;
+          var error = jqXHR.responseJSON || jqXHR.responseXML || jqXHR.responseText;
           if(typeof error == 'string') {
             try {
               error = JSON.parse(error);
             } catch(e) {}
           }
           console.error(error);
+          if(errhandler) {
+            errhandler.call(this, error, jqXHR, textStatus);
+          }
         }
       },
       success: function(data, textStatus, jqXHR) {
-        if(jqXHR.status != 200) return;
+        if(jqXHR.status != 200 || data.code != '200') return;
         callback.apply(this, arguments);
       }
     });
   }
 
   return {
-    init: function(type) {
+    init: function(settings) {
+      $.extend(true, options, settings);
       init_message_load_more();
       init_acknowledge_button();
       init_collapse_button();
